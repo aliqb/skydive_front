bd99af264d6d32cfd87f504886a1a3d55fd2f634
Mahdi <mahditouiserkani@yahoo.com>
2023-06-17 12:07:35 +0330

    Create grid component and add it to the user management

---
+++ src/components/shared/Grid.tsx
@@ -0,0 +1,50 @@
+import { Table } from "flowbite-react";
+import React from "react";
+interface GridProps {
+  data: Record<string, any>[];
+}
+
+const Grid: React.FC<GridProps> = ({ data }) => {
+  const columns = data.length > 0 ? Object.keys(data[0]) : [];
+
+  {
+    return (
+      <Table hoverable>
+        <Table.Head>
+          {columns.map((column) => (
+            <Table.HeadCell key={column}>{column}</Table.HeadCell>
+          ))}
+          <Table.HeadCell>
+            <span className="sr-only">Edit</span>
+          </Table.HeadCell>
+        </Table.Head>
+        <Table.Body className="divide-y">
+          {data.map((row, index) => (
+            <Table.Row
+              className="bg-white dark:border-gray-700 dark:bg-gray-800"
+              key={index}
+            >
+              {columns.map((column) => (
+                <Table.Cell
+                  className="whitespace-nowrap font-medium text-gray-900 dark:text-white"
+                  key={column}
+                >
+                  {row[column]}
+                </Table.Cell>
+              ))}
+              <Table.Cell>
+                <a
+                  className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
+                  href="/tables"
+                >
+                  <p>Edit</p>
+                </a>
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+    );
+  }
+};
+export default Grid;
--- src/pages/adminPanel/pages/UserManagement.tsx
+++ src/pages/adminPanel/pages/UserManagement.tsx
@@ -1,5 +1,23 @@
-const UserManagement = () => {
-  return <div>Hello</div>;
+import React from "react";
+import Grid from "../../../components/shared/Grid";
+
+const UserManagement: React.FC = () => {
+  const students = [
+    { id: 1, name: "Kate", age: 25, favFruit: "🍏" },
+    { id: 2, name: "Tom", age: 23, favFruit: "🍌" },
+    { id: 3, name: "Ann", age: 26, favFruit: "🍊" },
+    { id: 4, name: "Jack", age: 21, favFruit: "🍒" },
+    { id: 4, name: "Jack", age: 21, favFruit: "🍒" },
+    { id: 4, name: "Jack", age: 21, favFruit: "🍒" },
+    { id: 4, name: "Jack", age: 21, favFruit: "🍒" },
+    { id: 4, name: "Jack", age: 21, favFruit: "🍒" },
+    { id: 4, name: "Jack", age: 21, favFruit: "🍒" },
+  ];
+  return (
+    <>
+      <Grid data={students} />
+    </>
+  );
 };
 
 export default UserManagement;
