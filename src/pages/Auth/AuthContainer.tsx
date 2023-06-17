862fc1f34cf6bf7b5e0969e9a4c384a5ade3b6bd
Ali Qayyoomi <aliqbhastam@gmail.com>
2023-06-17 16:34:12 +0330

    navigate to home from auth if it was already logged in

--- src/pages/Auth/AuthContainer.tsx
+++ src/pages/Auth/AuthContainer.tsx
@@ -1,7 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useAppSelector } from "../../hooks/reduxHooks";
+import { useEffect, useState } from "react";
 
 export default function AuthContainer() {
-  return (
+  const isAuthenticated = useAppSelector((state) => state.auth.isAuthenticated);
+  const [wasAuthenticated, setWasAuthenticated] = useState<boolean>(false);
+  useEffect(() => {
+    const authDataJson = localStorage.getItem("authData");
+    setWasAuthenticated(!!authDataJson);
+  }, []);
+  return isAuthenticated || wasAuthenticated ? (
+    <Navigate to="/" />
+  ) : (
     <div className="container px-1 m-auto py-1 flex justify-center items-center min-h-full">
       <div className="w-full sm:w-5/6 flex flex-col items-center max-w-lg rounded-md border">
         <Outlet></Outlet>
