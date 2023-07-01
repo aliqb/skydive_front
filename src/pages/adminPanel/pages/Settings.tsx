40d2d585f96a022359b2a488ddb1368e705c0dd9
Mahdi <mahditouiserkani@yahoo.com>
2023-07-01 18:37:15 +0330

    Implement all the required code for user type section

--- src/pages/adminPanel/pages/Settings.tsx
+++ src/pages/adminPanel/pages/Settings.tsx
@@ -7,36 +7,38 @@ const Settings: React.FC = () => {
   const [selectedUserTypes, setSelectedUserTypes] = useState<UserType[]>([]);
   const [selectedTickets, setSelectedTickets] = useState<{
     [key in UserType]: UserTicket[];
-  }>({});
+  }>({
+    'همراه با مربی': [],
+    آزاد: [],
+    ویژه: [],
+  });
 
   const handleUserTypeClick = (userType: UserType) => {
     if (selectedUserTypes.includes(userType)) {
       setSelectedUserTypes(
         selectedUserTypes.filter((type) => type !== userType)
       );
-      setSelectedTickets((prevState) => {
-        const newState = { ...prevState };
-        delete newState[userType];
-        return newState;
-      });
     } else {
       setSelectedUserTypes([...selectedUserTypes, userType]);
-      setSelectedTickets((prevState) => ({ ...prevState, [userType]: [] }));
     }
   };
 
   const handleAddTicket = (userType: UserType, ticket: UserTicket) => {
-    setSelectedTickets((prevState) => ({
-      ...prevState,
-      [userType]: [...prevState[userType], ticket],
-    }));
+    setSelectedTickets((prevState) => {
+      const updatedTickets = { ...prevState };
+      updatedTickets[userType] = [...prevState[userType], ticket];
+      return updatedTickets;
+    });
   };
 
   const handleRemoveTicket = (userType: UserType, ticket: UserTicket) => {
-    setSelectedTickets((prevState) => ({
-      ...prevState,
-      [userType]: prevState[userType].filter((t) => t !== ticket),
-    }));
+    setSelectedTickets((prevState) => {
+      const updatedTickets = { ...prevState };
+      updatedTickets[userType] = prevState[userType].filter(
+        (t) => t !== ticket
+      );
+      return updatedTickets;
+    });
   };
 
   const userTypes: UserType[] = ['همراه با مربی', 'آزاد', 'ویژه'];
@@ -47,97 +49,110 @@ const Settings: React.FC = () => {
     ویژه: ['آزاد', 'چارتر', 'همراه با مربی', 'ویژه'],
   };
 
-  // const getUserTickets = (userType: UserType): UserTicket[] => {
-  //   return selectedTickets[userType] || [];
-  // };
-
   return (
     <div className="container mx-auto p-4">
       <div className="bg-gray-100 p-4">
-        <h2 className="text-lg font-bold mb-2">نوع کاربر</h2>
-        <ul>
+        <h2 className="text-2xl font-bold mb-4">نوع کاربر</h2>
+        <ul className="space-y-4">
           {userTypes.map((userType) => (
             <li
               key={userType}
-              className={`py-1 px-2 rounded-md mb-1 cursor-pointer ${
+              className={`flex flex-col ${
                 selectedUserTypes.includes(userType)
-                  ? 'bg-blue-200'
+                  ? 'bg-black-200 text-blue-600'
                   : 'bg-white'
               }`}
-              onClick={() => handleUserTypeClick(userType)}
+              style={{
+                border: '1px solid gray',
+                borderRadius: '0.5rem',
+              }}
             >
-              {userType}
+              <button
+                className={`p-4 text-lg font-bold w-full text-right ${
+                  selectedUserTypes.includes(userType) ? 'text-blue' : ''
+                }`}
+                onClick={() => handleUserTypeClick(userType)}
+              >
+                {userType}
+              </button>
+              {selectedUserTypes.includes(userType) && (
+                <ul className="space-y-2 p-4">
+                  {allowedTicketTypes[userType].map((ticketType) => (
+                    <li
+                      key={ticketType}
+                      className={`flex items-center ${
+                        selectedTickets[userType]?.includes(ticketType)
+                          ? 'text-blue-600'
+                          : 'text-black'
+                      }`}
+                    >
+                      <span
+                        className={`px-2 ml-4 py-1 rounded-md ${
+                          selectedTickets[userType]?.includes(ticketType)
+                            ? 'bg-green-200'
+                            : 'bg-white'
+                        }`}
+                      >
+                        {ticketType}
+                      </span>
+                      {!selectedTickets[userType]?.includes(ticketType) && (
+                        <button
+                          className="px-2 py-1 rounded-md bg-green-200 ml-2 flex items-center"
+                          onClick={() => handleAddTicket(userType, ticketType)}
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M12 4.5v15m7.5-7.5h-15"
+                            />
+                          </svg>
+                          <span className="ml-2">افزودن</span>
+                        </button>
+                      )}
+                      {selectedTickets[userType]?.includes(ticketType) && (
+                        <button
+                          className={`px-2 py-1 rounded-md bg-red-200 ml-2 flex items-center ${
+                            selectedTickets[userType]?.includes(ticketType)
+                              ? 'text-black'
+                              : ''
+                          }`}
+                          onClick={() =>
+                            handleRemoveTicket(userType, ticketType)
+                          }
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M19.5 12h-15"
+                            />
+                          </svg>
+                          <span className="ml-2">حذف</span>
+                        </button>
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              )}
             </li>
           ))}
         </ul>
       </div>
-      {selectedUserTypes.length > 0 && (
-        <div className="mt-4">
-          <h3 className="text-lg font-bold mb-2">نوع بلیت مرتبط</h3>
-          {selectedUserTypes.map((userType) => (
-            <div key={userType} className="mb-4">
-              <h4 className="font-bold mb-2">{userType}</h4>
-              <ul>
-                {allowedTicketTypes[userType].map((ticketType) => (
-                  <li key={ticketType} className="mb-2 flex items-center">
-                    <span
-                      className={`px-2 ml-4 py-1 rounded-md ${
-                        selectedTickets[userType]?.includes(ticketType)
-                          ? 'bg-green-200'
-                          : 'bg-white'
-                      }`}
-                    >
-                      {ticketType}
-                    </span>
-                    <button
-                      className="px-2 py-1 rounded-md bg-green-200 ml-2 flex items-center"
-                      onClick={() => handleAddTicket(userType, ticketType)}
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="w-6 h-6"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M12 4.5v15m7.5-7.5h-15"
-                        />
-                      </svg>
-                      <span className="ml-2">افزودن</span>
-                    </button>
-                    {selectedTickets[userType]?.includes(ticketType) && (
-                      <button
-                        className="px-2 py-1 rounded-md bg-red-200 ml-2 flex items-center"
-                        onClick={() => handleRemoveTicket(userType, ticketType)}
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={1.5}
-                          stroke="currentColor"
-                          className="w-6 h-6"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M19.5 12h-15"
-                          />
-                        </svg>
-                        <span className="ml-2">حذف</span>
-                      </button>
-                    )}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
     </div>
   );
 };
