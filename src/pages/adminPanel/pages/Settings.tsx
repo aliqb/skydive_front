1b3faeb66837e3b87e3769a2391847e998ea0932
Mahdi <mahditouiserkani@yahoo.com>
2023-07-01 16:17:31 +0330

    Implement the code with static data

--- src/pages/adminPanel/pages/Settings.tsx
+++ src/pages/adminPanel/pages/Settings.tsx
@@ -1,11 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
+
+type UserType = 'همراه با مربی' | 'آزاد' | 'ویژه';
 
 const Settings: React.FC = () => {
+  const userTypes: UserType[] = ['همراه با مربی', 'آزاد', 'ویژه'];
+
   return (
-    <>
-      <div>Settings</div>
-      <div>Hello</div>
-    </>
+    <div className="container mx-auto p-4">
+      <div className="bg-gray-100 p-4">
+        <h2 className="text-lg font-bold mb-2">انواع کاربر</h2>
+        <ul>
+          {userTypes.map((userType, index) => (
+            <li key={index} className="py-1 px-2 rounded-md bg-white mb-1">
+              {userType}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 };
 
