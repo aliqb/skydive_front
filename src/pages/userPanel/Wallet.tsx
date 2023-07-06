c3d4fa73dff3b61ad4d863f5a50ce827d27c4067
Mahdi <mahditouiserkani@yahoo.com>
2023-07-06 17:30:41 +0330

    Implement some changes

--- src/pages/userPanel/Wallet.tsx
+++ src/pages/userPanel/Wallet.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react';
-import { FaWallet } from 'react-icons/fa';
 import useApi from '../../hooks/useApi';
 import { BaseResponse } from '../../models/shared.models';
 import SDSpinner from '../../components/shared/Spinner';
+import SDCard from '../../components/shared/Card';
 
 interface WalletData {
   userId: string;
@@ -30,41 +30,39 @@ const Wallet: React.FC = () => {
   }, []);
 
   return (
-    <div className="flex items-center justify-center h-screen">
+    <SDCard className="flex items-center justify-center min-h-screen p-8 bg-red-500">
       {isPending ? (
         <div className="flex justify-center my-12">
           <SDSpinner size={20} />
         </div>
       ) : (
-        <div className="bg-white p-6 shadow rounded-lg">
-          <h2 className="text-xl font-bold mb-4">کیف پول کاربر</h2>
+        <SDCard className="shadow rounded-lg w-1/2 mb-16 sm:w-full sm:max-w-screen-md">
+          <h1 className="text-3xl font-bold mb-4">کیف پول کاربر</h1>
+
           <div className="flex items-center mb-4">
-            <FaWallet className="text-gray-500 mr-2 text-2xl" />
-            <span className="text-gray-600">User ID:</span>
-            <span className="text-gray-800 ml-2">
-              {walletData?.content.userId}
-            </span>
-          </div>
-          <div className="flex items-center mb-4">
-            <FaWallet className="text-gray-500 mr-2 text-2xl" />
-            <span className="text-gray-600">Balance:</span>
-            <span className="text-gray-800 ml-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 12a2.25 2.25 0 00-2.25-2.25H15a3 3 0 11-6 0H5.25A2.25 2.25 0 003 12m18 0v6a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 9m18 0V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v3"
+              />
+            </svg>
+
+            <span className="text-xl m-4 text-gray-600">موجودی :</span>
+            <span className="text-lg text-gray-800 ml-2">
               {walletData?.content.balance}
             </span>
           </div>
-          <div className="flex items-center mb-4">
-            <FaWallet className="text-gray-500 mr-2 text-2xl" />
-            <span className="text-gray-600">ID:</span>
-            <span className="text-gray-800 ml-2">{walletData?.content.id}</span>
-          </div>
-          <div className="flex items-center mt-4">
-            <FaWallet className="text-gray-500 mr-2 text-2xl" />
-            <span className="text-gray-600">Total:</span>
-            <span className="text-gray-800 ml-2">{walletData?.total}</span>
-          </div>
-        </div>
+        </SDCard>
       )}
-    </div>
+    </SDCard>
   );
 };
 
