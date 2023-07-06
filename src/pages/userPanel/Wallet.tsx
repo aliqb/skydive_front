8585d82c64b2099843aae4b223b6922f19276f52
Mahdi <mahditouiserkani@yahoo.com>
2023-07-06 16:47:57 +0330

    Implement the design and responsivity

--- src/pages/userPanel/Wallet.tsx
+++ src/pages/userPanel/Wallet.tsx
@@ -30,43 +30,41 @@ const Wallet: React.FC = () => {
   }, []);
 
   return (
-    <>
-      (
-      {isPending && (
+    <div className="flex items-center justify-center h-screen">
+      {isPending ? (
         <div className="flex justify-center my-12">
           <SDSpinner size={20} />
         </div>
+      ) : (
+        <div className="bg-white p-6 shadow rounded-lg">
+          <h2 className="text-xl font-bold mb-4">کیف پول کاربر</h2>
+          <div className="flex items-center mb-4">
+            <FaWallet className="text-gray-500 mr-2 text-2xl" />
+            <span className="text-gray-600">User ID:</span>
+            <span className="text-gray-800 ml-2">
+              {walletData?.content.userId}
+            </span>
+          </div>
+          <div className="flex items-center mb-4">
+            <FaWallet className="text-gray-500 mr-2 text-2xl" />
+            <span className="text-gray-600">Balance:</span>
+            <span className="text-gray-800 ml-2">
+              {walletData?.content.balance}
+            </span>
+          </div>
+          <div className="flex items-center mb-4">
+            <FaWallet className="text-gray-500 mr-2 text-2xl" />
+            <span className="text-gray-600">ID:</span>
+            <span className="text-gray-800 ml-2">{walletData?.content.id}</span>
+          </div>
+          <div className="flex items-center mt-4">
+            <FaWallet className="text-gray-500 mr-2 text-2xl" />
+            <span className="text-gray-600">Total:</span>
+            <span className="text-gray-800 ml-2">{walletData?.total}</span>
+          </div>
+        </div>
       )}
-      )
-      <div className="bg-white p-6 shadow rounded-lg">
-        <h2 className="text-xl font-bold mb-4">کیف پول کاربر</h2>
-        <div className="flex items-center mb-4">
-          <FaWallet className="text-gray-500 mr-2 text-2xl" />
-          <span className="text-gray-600">User ID:</span>
-          <span className="text-gray-800 ml-2">
-            {walletData?.content.userId}
-          </span>
-        </div>
-        <div className="flex items-center mb-4">
-          <FaWallet className="text-gray-500 mr-2 text-2xl" />
-          <span className="text-gray-600">Balance:</span>
-          <span className="text-gray-800 ml-2">
-            {walletData?.content.balance}
-          </span>
-        </div>
-        <div className="flex items-center mb-4">
-          <FaWallet className="text-gray-500 mr-2 text-2xl" />
-          <span className="text-gray-600">ID:</span>
-          <span className="text-gray-800 ml-2">{walletData?.content.id}</span>
-        </div>
-        <div className="flex items-center mt-4">
-          <FaWallet className="text-gray-500 mr-2 text-2xl" />
-          <span className="text-gray-600">Total:</span>
-          <span className="text-gray-800 ml-2">{walletData?.total}</span>
-        </div>
-        {isPending && <div className="text-gray-500 mt-4">Loading...</div>}
-      </div>
-    </>
+    </div>
   );
 };
 
