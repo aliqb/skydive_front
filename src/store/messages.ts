81dbbf9216f9f2861a3e0aca5796bbea8d84fa51
Ali Qayyoomi <aliqbhastam@gmail.com>
2023-07-07 18:08:23 +0330

    read message in redux

--- src/store/messages.ts
+++ src/store/messages.ts
@@ -2,14 +2,16 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { UserMessage, UserMessageResponse } from "../models/messages.models";
 import { axiosIntance } from "../hooks/useApi";
 import { AxiosError, AxiosResponse } from "axios";
-import {  PagingParams } from "../models/shared.models";
+import {  BaseResponse, PagingParams } from "../models/shared.models";
+import { toast } from "react-toastify";
 
 interface MessagesState{
     messages:UserMessage[],
     total: number,
     unReadCount: number,
     isLoading: boolean,
-    error: string
+    error: string,
+    changingMessageid:string | null;
 }
 
 const initialState : MessagesState = {
@@ -17,7 +19,8 @@ const initialState : MessagesState = {
     unReadCount: 0,
     total: 0,
     error: '',
-    isLoading: false
+    isLoading: false,
+    changingMessageid: null
 }
 
 export const fetchMessages = createAsyncThunk('messages/fetchMessages',async (pagingParmas:Partial<PagingParams> )=>{
@@ -37,6 +40,23 @@ export const fetchMessages = createAsyncThunk('messages/fetchMessages',async (pa
           message: string;
         }>;
         throw new Error(axiosError.response?.data.message || "");
+      }
+})
+
+export const readMessage = createAsyncThunk('messages/readMessage',async (messageId: string)=>{
+    try {
+        const response = await axiosIntance.put<
+          null,
+          AxiosResponse<BaseResponse<null>>
+        >(`/UserMessages/${messageId}`);
+        return response.data;
+      } catch (error) {
+        const axiosError: AxiosError<{ message: string }> = error as AxiosError<{
+          message: string;
+        }>;
+        const message = axiosError.response?.data.message || "";
+        toast.error(message);
+        throw new Error(message);
       }
 })
 
@@ -60,6 +80,24 @@ const messgeSlice = createSlice({
             state.error = action.error.message || "";
             state.isLoading = false;
             state.messages = []
+        })
+        //read
+        builder.addCase(readMessage.pending,(state,action)=>{
+            state.changingMessageid = action.meta.arg
+        })
+        builder.addCase(readMessage.fulfilled,(state)=>{
+            const messageId = state.changingMessageid;
+            state.messages = state.messages.map(message=>{
+                if(message.id === messageId){
+                    return {...message, visited:true}
+                }
+                return {...message}
+            })
+            state.changingMessageid = null;
+            state.unReadCount = state.unReadCount -1;
+        })
+        builder.addCase(readMessage.rejected,(state)=>{
+            state.changingMessageid = null;
         })
     }
 })
