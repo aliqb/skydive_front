b2a4080d1668dbe8f0ed143bd462fa621aabe544
Mahdi <mahditouiserkani@yahoo.com>
2023-07-10 13:20:29 +0330

    Fix the error on Home.jsx

--- src/pages/userPanel/Home.tsx
+++ src/pages/userPanel/Home.tsx
@@ -16,7 +16,7 @@ const Home: React.FC = () => {
   const [links, setLinks] = useState<HomeLinkProps[]>([]);
   const name = useAppSelector((state) => state.auth.name);
   const authState = useAppSelector((state) => state.auth);
-
+  
   const defaultLinks: HomeLinkProps[] = [
     {
       tilte: 'رویدادها',
@@ -27,12 +27,16 @@ const Home: React.FC = () => {
       href: '',
     },
     {
-      tilte: 'بلیت‌های من',
-      href: '/tickets',
-    },
-    {
       tilte: 'سوابق پرش',
       href: '/jumps',
+    },
+    {
+      tilte: 'کیف پول',
+      href: '/wallet',
+    },
+    {
+      tilte: 'بلیت‌های من',
+      href: '/tickets',
     },
     {
       tilte: 'سوابق تراکنش ها',
@@ -79,51 +83,6 @@ const Home: React.FC = () => {
       }
     );
   }, [sendSettingsRequest]);
-
-  return (
-    <SDCard className="flex justify-center">
-      <main className="w-full max-w-xl flex flex-col justify-center">
-        <div className="mb-6">
-          <div className="text-center">
-            <p className="mb-2 text-lg">{name}</p>
-            <div className="flex justify-center items-center">
-              <p className="ml-2 font-semibold text-sm">وضعیت حساب کاربری:</p>
-              <p
-                className={`${statusBgColorMap.get(
-                  authState.userStatus
-                )} text-xs  inline py-1 px-3 rounded-2xl`}
-              >
-                {authState.userStatusDisplay}
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="flex flex-wrap justify-center">
-        {
-          tilte: 'رویدادها',
-          href: '/events',
-        },
-        {
-          tilte: 'قوانین و شرایط',
-          href: response.content.termsAndConditionsUrl,
-          target: '_blank'
-        },
-        {
-          tilte: 'بلیت‌های من',
-          href: '/tickets',
-        },
-        {
-          tilte: 'سوابق پرش',
-          href: '/jumps',
-        },
-        {
-          tilte: 'سوابق تراکنش ها',
-          href: '/transactions',
-        },
-      ])
-    })
-  },[sendSettingsRequest])
 
   return (
     <SDCard className="flex justify-center">
