1a305be99832638ca28e08bff69124992513a8de
Ali Qayyoomi <aliqbhastam@gmail.com>
2023-08-22 10:06:10 +0330

    config peimary2 color to be same a flowbite color

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,8 +21,18 @@ export default {
           DEFAULT: 'rgb(191 115 0)'
         },
         primary2:{
-          ...colors.blue,
-          DEFAULT: colors.blue[900]
+          "50":"#EBF5FF",
+          "100":"#E1EFFE",
+          "200":"#C3DDFD",
+          "300":"#A4CAFE",
+          "400":"#76A9FA",
+          "500":"#3F83F8",
+          "600":"#1C64F2",
+          "700":"#1A56DB",
+          "800":"#1E429F",
+          "900":"#233876",
+          DEFAULT: '#233876'
+          
         }
       },
       boxShadow:{
