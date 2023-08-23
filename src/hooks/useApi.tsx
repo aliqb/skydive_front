b3b4cb8b13d2a77697d4ef14230516d3e79fbe8e
Mahdi <mahditouiserkani@yahoo.com>
2023-08-23 13:53:45 +0330

    Implement : handle the 401 error

--- src/hooks/useApi.tsx
+++ src/hooks/useApi.tsx
@@ -3,49 +3,60 @@ import { useCallback, useState, useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "./reduxHooks";
 import { BaseResponse } from "../models/shared.models";
 import { authActions } from "../store/auth";
+import { useNavigate } from 'react-router-dom';
 export const axiosIntance = axios.create({
   baseURL: import.meta.env.VITE_BASE_API_URL,
 });
 export default function useAPi<
   T,
-  R = BaseResponse<any>,
+  R = BaseResponse<T>,
   ErrorType = { message: string }
 >() {
+  const navigate = useNavigate();
   const [isPending, setIsPending] = useState<boolean>(false);
   const [errors, setErrors] = useState<ErrorType | undefined>(undefined);
   const [data, setData] = useState<R | null>(null);
   const token = useAppSelector((state) => state.auth.token);
   const dispatch = useAppDispatch();
-  const sendRequest = useCallback(async function (
-    config: AxiosRequestConfig<T>,
-    applyData?: (data: R) => void,
-    onError?: (error: ErrorType | undefined) => void
-  ) {
-    setIsPending(true);
-    try {
-      const response = await axiosIntance.request<T, AxiosResponse<R>>(config);
-      setData(response.data);
-      if (applyData) {
-        applyData(response.data);
+  const sendRequest = useCallback(
+    async function (
+      config: AxiosRequestConfig<T>,
+      applyData?: (data: R) => void,
+      onError?: (error: ErrorType | undefined) => void
+    ) {
+      setIsPending(true);
+      try {
+        const response = await axiosIntance.request<T, AxiosResponse<R>>(
+          config
+        );
+        setData(response.data);
+        if (applyData) {
+          applyData(response.data);
+        }
+        setErrors(undefined);
+      } catch (error) {
+        const axiosError: AxiosError<ErrorType> =
+          error as AxiosError<ErrorType>;
+        if (axiosError.response?.status === 401) {
+          navigate('/auth');
+          return;
+        }
+        setErrors(axiosError.response?.data);
+        if (onError) {
+          onError(axiosError.response?.data);
+        }
+      } finally {
+        setIsPending(false);
       }
-      setErrors(undefined)
-    } catch (error) {
-      const axiosError: AxiosError<ErrorType> = error as AxiosError<ErrorType>;
-      setErrors(axiosError.response?.data);
-      if (onError) {
-        onError(axiosError.response?.data);
-      }
-    } finally {
-      setIsPending(false);
-    }
-  },
-  []);
+    },
+    [navigate]
+  );
 
   useEffect(() => {
     if (token) {
-      axiosIntance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+      axiosIntance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
       dispatch(authActions.setHttpHeader());
     }
-  }, [token,dispatch]);
+  }, [token, dispatch]);
   return { isPending, errors, sendRequest, data };
 }
